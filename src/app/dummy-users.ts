c87e767b4638d1ed51b6f457336d96beaad2f7da
Qinyue-W <Qinyue.Wang@dal.ca>
2025-10-03 16:24:08 -0400

    add component inputs

---
+++ src/app/dummy-users.ts
@@ -0,0 +1,32 @@
+export const DUMMY_USERS =[
+    {
+        id:'u1',
+        name:'Jasmine W',
+        avatar:'user-1.jpg',
+    },
+    {
+        id:'u2',
+        name:'Emily T',
+        avatar:'user-2.jpg',
+    },
+    {
+        id:'u3',
+        name:'Marcus J',
+        avatar:'user-3.jpg',
+    },
+    {
+        id:'u4',
+        name:'David M',
+        avatar:'user-4.jpg',
+    },
+    {
+        id:'u5',
+        name:'Priya P',
+        avatar:'user-5.jpg',
+    },
+    {
+        id:'u6',
+        name:'Arjun S',
+        avatar:'user-6.jpg',
+    }
+]
